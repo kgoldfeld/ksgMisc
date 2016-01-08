b561b05edb413a6f2e045f7d50b50fb80ee96d2b
Keith Goldfeld <keith.goldfeld@nyumc.org>
2016-01-08 15:30:44 -0500

    The fourth try to commit

--- R/rwExcel.R
+++ R/rwExcel.R
@@ -1,11 +1,11 @@
-#  readExcel
+#  readExcel by ksg
 
 #' Reads from clipboard into data.frame: windows
 #'
 #' Wrapper for read.table that reads from the clipboard into a data.frame with
 #' or without the header - works only on Windows.
 #'
-#' @param header True or False.
+#' @param header True/False.
 #' @param ... extra arguments
 #' @return A data frame
 #' @export
