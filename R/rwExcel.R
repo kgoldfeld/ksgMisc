cfe13870e040b8fd9b6b4dab23413ee2e9d6ba34
Keith Goldfeld <keith.goldfeld@nyumc.org>
2016-01-08 15:33:06 -0500

    This is the fifth try

--- R/rwExcel.R
+++ R/rwExcel.R
@@ -5,7 +5,7 @@
 #' Wrapper for read.table that reads from the clipboard into a data.frame with
 #' or without the header - works only on Windows.
 #'
-#' @param header True/False.
+#' @param header True or False.
 #' @param ... extra arguments
 #' @return A data frame
 #' @export
