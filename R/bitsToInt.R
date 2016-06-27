cae138f206c3b675a5e4fe31ba6461af832b0a70
Keith Goldfeld <keith.goldfeld@nyumc.org>
2016-06-27 16:59:48 -0400

    Adding function to convert bits to integer

---
+++ R/bitsToInt.R
@@ -0,0 +1,17 @@
+#' Converts vector of 1's and 0's to integer ...
+#'
+#' @param x Vector of 1's or zero's
+#' @return Integer value of vector
+#' @export
+
+bitsToInt<-function(x) {
+
+  if (is.character(x)) {
+    y <- as.integer(strsplit(x, split=NULL)[[1]])
+  } else if (is.numeric(x)) {
+    y <- x
+  }
+
+  packBits(rev(c(rep(FALSE, 32-length(y)%%32), as.logical(y))), "integer")
+
+}
