f86fe9333009d0ca2eea38f93e5826aa2a746a32
Keith Goldfeld <keith.goldfeld@nyumc.org>
2016-08-15 11:31:35 -0400

    Updating sumData to generate table reports of factor variables and to report missing data for numeric variables.

--- R/sumdata.R
+++ R/sumdata.R
@@ -12,33 +12,59 @@ sumData <- function(dt) {
   dtNames <- names(dt)
   n <- length(dtNames)
 
+  varname <- list()
   N <- list()
+  missing <- list()
   means <- list()
   sds <- list()
   maxs <- list()
   mins <- list()
+  median <- list()
+
+  table.t <- list()
+
+  j <- 0
+  k <- 0
 
   for (i in (1:n)) {
-    N[[i]] <- sum(!is.na(dt[,get(dtNames[i])]))
-    maxs[[i]] <- max(dt[,get(dtNames[i])], na.rm = TRUE)
-    mins[[i]] <- min(dt[,get(dtNames[i])], na.rm = TRUE)
-    if (is.numeric(dt[,get(dtNames[i])])) {
-      means[[i]] <- mean(dt[,get(dtNames[i])], na.rm = TRUE)
-      sds[[i]] <- sd(dt[,get(dtNames[i])], na.rm = TRUE)
-    } else {
-      means[[i]]=NA
-      sds[[i]]=NA
+
+    colI <- dt[,get(dtNames[i])]
+
+    if (is.numeric(colI)) {
+
+      j <- j + 1
+
+      varname[[j]] <- dtNames[i]
+      N[[j]] <- sum(!is.na(colI))
+      missing[[j]] <- sum(is.na(colI))
+      maxs[[j]] <- max(colI, na.rm = TRUE)
+      mins[[j]] <- min(colI, na.rm = TRUE)
+      means[[j]] <- mean(colI, na.rm = TRUE)
+      sds[[j]] <- sd(colI, na.rm = TRUE)
+      median[[j]] <- median(colI, na.rm = TRUE)
+
+    } else if (is.factor(colI)) {
+
+      k <- k + 1
+      ct <- data.table(var=colI)
+      tt <- ct[,.N,,keyby=var][, .(var, count=N, pct = round(N/sum(N),3))]
+      setnames(tt,"var", dtNames[i])
+
+      table.t[[k]] <- tt
+
     }
   }
 
-  sumdat <- data.table::data.table(varName = dtNames,
+  sumdat <- data.table::data.table(varName = unlist(varname),
                                    N = unlist(N),
+                                   missing = unlist(missing),
                                    min = unlist(mins),
                                    max = unlist(maxs),
                                    mean = unlist(means),
-                                   sd = unlist(sds)
+                                   sd = unlist(sds),
+                                   median = unlist(median)
   )
 
-  return(sumdat)
-}
+  return(list(sumdat, table.t))
 
+}
