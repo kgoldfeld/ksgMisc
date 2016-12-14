18fb18e5c88951f41c83a8f71f095161054ab110
Keith Goldfeld <keith.goldfeld@nyumc.org>
2016-12-14 15:03:05 -0500

    Exporting show_condition()

--- R/show_condition.R
+++ R/show_condition.R
@@ -1,3 +1,13 @@
+#' Screens functions for messages
+#'
+#' Checks to see if function call returns error
+#'
+#' @param code Function call
+#' @return If the function results in a message, the function returns a string
+#' indicating "error", "warning", or "message"; otherwise the object of the
+#' function call is returned.
+#' @export
+#'
 show_condition <- function(code) {
   tryCatch(code,
            error = function(c) "error",
