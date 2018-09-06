c5612ae4cb1d2c666a0db9e9261cb754f8a31335
Keith Goldfeld <keith.goldfeld@nyumc.org>
2018-09-06 13:14:17 -0400

    updating replaceInFiles

--- R/replaceInFiles.R
+++ R/replaceInFiles.R
@@ -3,25 +3,30 @@
 #' @param directory Location of files to be changed
 #' @param oldtext Current text
 #' @param newtext New text
+#' @param verbose Indicates whether updated files should be printed. Defaults to TRUE.
 #' @return Nothing
 #' @export
 #'
-replaceInFiles <- function(directory, oldtext, newtext) {
+replaceInFiles <- function(directory, oldtext, newtext, verbose = TRUE) {
 
   origdir <- getwd()
   setwd(directory)
   filenames <- list.files()
 
+  if (verbose) cat("Files updated ...\n\n")
+
   for( f in filenames ){
 
     x <- readLines(f)
-    y <- gsub( oldtext, newtext, x )
-    cat(y, file = f, sep = "\n")
 
+    if (length(grep(oldtext, x)) != 0) {
+      y <- gsub( oldtext, newtext, x )
+      cat(y, file = f, sep = "\n")
+      if (verbose) cat(f, "\n")
+    }
   }
 
+  cat("\n")
   setwd(origdir)
-
-  cat("Files updated ...\n")
 
 }
